96986542496b45dbddb355627f8b12c6df36e266
Yikey <gtc122004@gmail.com>
2018-06-01 17:03:10 -0400

    Update and rename .gitkeep to DBL_STATS_LOADER.js

---
+++ loaders/DBL_STATS_LOADER.js
@@ -0,0 +1,11 @@
+module.exports.run = function(client) {
+  setInterval(() => {
+		try {
+			dbl.postStats(bot.guilds.size);
+			console.log("postStats is Done");
+		} catch (error) {
+			console.log("postStats error: " + error);
+		}
+	}, 1800000);
+  //30 min
+};
